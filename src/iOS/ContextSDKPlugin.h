5b29d158152bcac625dd66b14ddd19669ac6382d
waldemar <waldemar.krumrick@darwoft.com>
2013-10-16 17:54:08 -0300

    New revision

---
+++ src/iOS/ContextSDKPlugin.h
@@ -0,0 +1,22 @@
+//
+//  ContextSDK.h
+//  Hydrogen
+//
+//  Created by Waldemar Krumrick on 10/1/13.
+//
+//
+
+#import <Cordova/CDV.h>
+#import "Context.h"
+
+@interface ContextSDKPlugin : CDVPlugin <OnStatusCallback>
+
+@property(nonatomic,strong) NSString *callbackId;
+
+- (void)initWithServicesList:(CDVInvokedUrlCommand *)command;
+
+- (void)stopServices:(CDVInvokedUrlCommand *)command;
+
+- (void)getServicesList:(CDVInvokedUrlCommand *)command;
+
+@end
